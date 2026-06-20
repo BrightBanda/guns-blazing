d939649ebcdc5d186f3ab72c6760068e675b85b7
Daylight <nytemod@gmail.com>
2026-06-20 11:44:49 +0200

    fixed ui on full screen

--- src/core/hud.gd
+++ src/core/hud.gd
@@ -1,24 +1,24 @@
 extends CanvasLayer
 
-@onready var ammo_label:Label = $Ammo
-@onready var reload_indicator:Label = $CenterContainer/ReloadIndicator
+@onready var ammo_label:Label = $UI_Root/MarginContainer/Ammo
+@onready var reload_indicator:Label = $UI_Root/CenterContainer/ReloadIndicator
 
 func _ready() -> void:
 	if not reload_indicator:
-		reload_indicator = $CenterContainer/ReloadIndicator
+		reload_indicator = $UI_Root/CenterContainer/ReloadIndicator
 	reload_indicator.visible = false
 	
 func on_ammo_changed(current:int,max:int):
 	if not ammo_label:
-		ammo_label = $Ammo
+		ammo_label = $UI_Root/MarginContainer/Ammo
 	ammo_label.text = str(current) + " / " + str(max)
 	
 func on_reload_started():
 	if not reload_indicator:
-		reload_indicator = $CenterContainer/ReloadIndicator
+		reload_indicator = $UI_Root/CenterContainer/ReloadIndicator
 	reload_indicator.visible = true
 
 func on_reload_finished():
 	if not reload_indicator:
-		reload_indicator = $CenterContainer/ReloadIndicator
+		reload_indicator = $UI_Root/CenterContainer/ReloadIndicator
 	reload_indicator.visible = false
